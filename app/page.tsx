3f57f4899cb070004cb8c79af2b8c312d4947acd
Botuca <botucacontato@gmail.com>
2023-10-03 00:43:22 -0300

    added init video post

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,30 @@
 'use client';
+import ClientOnly from './components/ClientOnly';
+import PostMain from './components/PostMain';
 import MainLayout from './layouts/MainLayout';
 
 export default function Home() {
   return (
     <div>
       <MainLayout>
-        <div>HOME</div>
+        <div className="mt-[80px] w-[calc(100%-90px)] max-w-[690px] ml-auto">
+          <ClientOnly>
+            <PostMain
+              post={{
+                id: '123',
+                user_id: '456',
+                video_url: '/video.mp4',
+                text: 'botucson',
+                created_at: '31/02',
+                profile: {
+                  user_id: '456',
+                  name: 'Usuario 1',
+                  image: 'https://placehold.co/100',
+                },
+              }}
+            />
+          </ClientOnly>
+        </div>
       </MainLayout>
     </div>
   );
